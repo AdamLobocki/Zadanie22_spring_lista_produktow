c23465d0042594f48e24224722de18bc6b5ff3e3
Jaszczyk <47436470+Jaszczyk@users.noreply.github.com>
2019-07-17 14:15:17 +0200

    Zadanie zrobione

---
+++ src/main/java/pl/al/zadanie22_spring_lista_produktow/Zadanie22SpringListaProduktowApplication.java
@@ -0,0 +1,13 @@
+package pl.al.zadanie22_spring_lista_produktow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie22SpringListaProduktowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie22SpringListaProduktowApplication.class, args);
+    }
+
+}
